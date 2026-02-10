b005657e080b33b8c0a806f1f0605e33050f1d0f
oldmlysenko <oldmlysenko@gmail.com>
2026-02-10 22:22:12 +0100

    enemy components were bundled

--- src/game/enemies/doordash.rs
+++ src/game/enemies/doordash.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, transform::components};
+use crate::game::enemies::{Difficulty, Enemy, EnemyState};
 
 pub(super) fn plugin(app: &mut App) {
     
@@ -7,3 +8,12 @@ pub(super) fn plugin(app: &mut App) {
 #[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 struct Doordash;
+
+pub fn doordash(difficulty: i8) -> impl Bundle{
+    (
+        Name::new("Doordash"),
+        Doordash,
+        Difficulty::new(difficulty),
+        Enemy::new(),
+    )
+}
--- src/game/enemies/washer.rs
+++ src/game/enemies/washer.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, transform::components};
+use crate::game::enemies::{Difficulty, Enemy, EnemyState};
 
 pub(super) fn plugin(app: &mut App) {
     
@@ -7,3 +8,12 @@ pub(super) fn plugin(app: &mut App) {
 #[derive(Component, Reflect, Debug)]
 #[reflect(Component)]
 struct Washer;
+
+pub fn washer(difficulty: i8) -> impl Bundle{
+    (
+        Name::new("Washer"),
+        Washer,
+        Difficulty::new(difficulty),
+        Enemy::new(),
+    )
+}
