3916706ae9b78f9cce1dbdb04de6ff5e6ae687bd
shap-po <50052124+shap-po@users.noreply.github.com>
2026-02-15 15:19:24 +0200

    chore: Remove test interactions

--- src/game/interactions.rs
+++ src/game/interactions.rs
@@ -7,25 +7,4 @@ pub(super) fn plugin(app: &mut App) {
         require_markers: false,
         picking_mode: SpritePickingMode::AlphaThreshold(0.01),
     });
-
-    app.add_systems(Startup, setup);
-}
-
-fn setup(mut commands: Commands) {
-    commands.add_observer(observe_picking::<Pointer<Over>>());
-    commands.add_observer(observe_picking::<Pointer<Out>>());
-    commands.add_observer(observe_picking::<Pointer<Press>>());
-    commands.add_observer(observe_picking::<Pointer<Release>>());
-}
-
-fn observe_picking<E>() -> impl Fn(On<E>, Query<&mut Sprite>)
-where
-    E: EntityEvent + std::fmt::Debug + Clone + Reflect,
-{
-    move |ev, sprites| {
-        let Ok(sprite) = sprites.get(ev.event_target()) else {
-            return;
-        };
-        println!("Observed {:?} on {:?}", ev.event_key(), sprite.image.path());
-    }
 }
