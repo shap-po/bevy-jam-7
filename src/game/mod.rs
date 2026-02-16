e179c5e4edf9e5a17c73f1973bfd81f3021c045c
shap-po <50052124+shap-po@users.noreply.github.com>
2026-02-16 05:19:35 +0200

    fix: Dev assertions

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -25,10 +25,7 @@ pub(super) fn plugin(app: &mut App) {
     app.add_systems(OnEnter(Screen::Gameplay), reset_timer);
     app.add_systems(
         Update,
-        (
-            #[cfg(debug_assertions)]
-            tick_timer,
-        )
+            tick_timer
             .run_if(in_state(Screen::Gameplay))
             .in_set(AppSystems::Update)
             .in_set(PausableSystems),
