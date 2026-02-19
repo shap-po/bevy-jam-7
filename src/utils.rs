1c6adad097c850ccacc117d4b5a3e89302d58091
shap-po <50052124+shap-po@users.noreply.github.com>
2026-02-19 17:51:18 +0200

    feat: Add DeferredSprite

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::*;
+use bevy::{
+    ecs::{lifecycle::HookContext, world::DeferredWorld},
+    prelude::*,
+};
 
 pub trait SetImage {
     /// Set the image if not already set
@@ -10,5 +13,36 @@ impl SetImage for Sprite {
         if self.image != *image {
             self.image = image.clone();
         }
+    }
+}
+
+#[derive(Component)]
+#[require(Sprite)]
+#[component(on_add = deferred_sprite_on_add::<R>)]
+/// Allows selecting sprite image without passing around the assets.
+pub struct DeferredSprite<R>(Box<dyn Fn(&R) -> Handle<Image> + Send + Sync + 'static>)
+where
+    R: Resource;
+
+impl<R> DeferredSprite<R>
+where
+    R: Resource,
+{
+    pub fn new(image_getter: impl Fn(&R) -> Handle<Image> + Send + Sync + 'static) -> Self {
+        Self(Box::new(image_getter))
+    }
+}
+
+fn deferred_sprite_on_add<R>(mut world: DeferredWorld, HookContext { entity, .. }: HookContext)
+where
+    R: Resource,
+{
+    let resource = world.get_resource::<R>().unwrap(); // panic if no resource
+    let Some(deferred_sprite) = world.get::<DeferredSprite<R>>(entity) else {
+        return;
+    };
+    let image = deferred_sprite.0(resource);
+    if let Some(mut sprite) = world.get_mut::<Sprite>(entity) {
+        sprite.image = image;
     }
 }
